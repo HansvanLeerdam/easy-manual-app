afe2f282ee38d991cfde1efbad4d76616c7bc9fa
Hans <hans123@gmail.com>
2025-10-18 18:29:34 +0200

    Fix Firebase storage bucket

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com", // harmless if unused
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCMi13Ci2AXdtVePTMIK1oJLrT61aTyHp0",
+  authDomain: "easy-manual-app.firebaseapp.com",
+  projectId: "easy-manual-app",
+  storageBucket: "easy-manual-app.appspot.com",
+  messagingSenderId: "255572942841",
+  appId: "1:255572942841:web:5a705fd1d559a13b82a728"
 };
 
 const app = initializeApp(firebaseConfig);
